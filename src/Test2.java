a29cb9745dd1246d9a6613494358eb35b4948ca9
Administrator <Administrator@telco11.fyre.ibm.com>
2021-12-11 20:33:19 -0800

    committing Test2 to branchX

---
+++ src/Test2.java
@@ -0,0 +1,15 @@
+
+public class Test2 {
+	public static void main(String [] args) {
+		
+		String day = "yes";
+		//action
+		if(day.equals("yes") ) {
+			System.out.println("Good day to you sir!!");
+		} else {
+			System.out.println("Good night sir!!");
+		}
+		
+	}
+
+}
