c444142e698afe5ca93e6838c19ba7a6029d2d18
bparasuram <paras.balakrishnan@gmail.com>
2021-12-12 08:12:48 +0530

    First commit

---
+++ src/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+	public static void main(String [] args) {
+		
+		String day = "yes";
+		if(day.equals("yes") ) {
+			System.out.println("Good day to you!!");
+		} else {
+			System.out.println("Good night!!");
+		}
+		
+	}
+
+}
